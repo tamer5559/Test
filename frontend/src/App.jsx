98fc834bb051ab2f5ac43aa854b1f8953ddc45a6
tamer5559 <koctamer5559@gmail.com>
2026-02-15 17:34:12 +0300

    Create App.jsx for media downloader component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    // TODO: Implement media downloader features
+
+    return (
+        <div>
+            <h1>Media Downloader</h1>
+            {/* Add components or functionality here */}
+        </div>
+    );
+};
+
+export default App;
